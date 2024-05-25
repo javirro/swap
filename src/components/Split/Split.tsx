109b9aa4221858d912c295c7a167601fc0eae830
idcetim <javirofe96@gmail.com>
2024-05-25 07:30:43 +0200

    Add elements for split section

--- src/components/Split/Split.tsx
+++ src/components/Split/Split.tsx
@@ -5,6 +5,7 @@ import { getBalance } from "../../blockchain/SwapMethods/getBalance"
 import { EIP1193Provider } from "../../types/Metamask"
 
 import "./Split.css"
+import { blockchain } from "../../blockchain"
 
 interface SplitProps {
   userAccount: string
@@ -15,7 +16,14 @@ interface SplitProps {
 const Split = ({ userAccount, chainId, provider }: SplitProps) => {
   const [amount, setAmount] = useState<string>("")
   const [from, setFrom] = useState<string>("bnb")
+  const [to1, setTo1] = useState<string>("eth")
+  const [to2, setTo2] = useState<string>("usdt")
+  const [percentage1, setPercentage1] = useState<number>(50)
+  const [percentage2, setPercentage2] = useState<number>(50)
   const [balance, setBalance] = useState<Balance>({ weiBalance: "", ethBalance: "" })
+  const tokens = blockchain.tokens.find(token => token.chainId === chainId)?.tokens as Object
+  const tokensNames: string[] = Object.keys(tokens)
+  const filteredTokensNameTo = tokensNames.filter(t => t.toLowerCase() !== from.toLowerCase())
 
   useEffect(() => {
     if (!provider) return
@@ -29,6 +37,23 @@ const Split = ({ userAccount, chainId, provider }: SplitProps) => {
   const handleMaxAmount = () => {
     setAmount(balance?.ethBalance)
   }
+  const handleToChange = (e: React.ChangeEvent<HTMLSelectElement>, token: string) => {
+    const value: string = e.target.value
+    if (token === "to1") setTo1(value.toLowerCase())
+    else setTo2(value.toLowerCase())
+  }
+
+  const handlePercentage = (e: React.ChangeEvent<HTMLInputElement>, token: string) => {
+    const value: number = Number(e.target.value)
+    if (token === "to1") {
+      setPercentage1(value)
+      setPercentage2(100 - value)
+    } else {
+      setPercentage2(value)
+      setPercentage1(100 - value)
+    }
+  }
+
   return (
     <section id="split">
       <div className="input-container">
@@ -41,6 +66,29 @@ const Split = ({ userAccount, chainId, provider }: SplitProps) => {
           setFrom={setFrom}
           handleMaxAmount={handleMaxAmount}
         />
+        <section className="to-section">
+          <h4>To</h4>
+          <div>
+            <select onChange={e => handleToChange(e, "to1")}>
+              {filteredTokensNameTo.map(t => (
+                <option value={t.toUpperCase()} key={t} selected={t === to1}>
+                  {t.toUpperCase()}
+                </option>
+              ))}
+            </select>
+            <input type="number" placeholder="0.0" value={percentage1} onChange={e => handlePercentage(e, "to1")} />
+          </div>
+          <div>
+            <select onChange={e => handleToChange(e, "to2")}>
+              {filteredTokensNameTo.map(t => (
+                <option value={t.toUpperCase()} key={t} selected={t === to2}>
+                  {t.toUpperCase()}
+                </option>
+              ))}
+            </select>
+            <input type="number" placeholder="0.0" step={"0.01"} max="100" min="0" value={percentage2} onChange={e => handlePercentage(e, "to2")} />
+          </div>
+        </section>
       </div>
     </section>
   )

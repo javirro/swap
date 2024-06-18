f954279d0996dc9033121df64d0505b5cf2ce716
idcetim <javirofe96@gmail.com>
2024-06-18 17:43:25 +0200

    Add splitEthContract address and ABI

--- src/blockchain/contracts/index.ts
+++ src/blockchain/contracts/index.ts
@@ -2,10 +2,12 @@ import pancakeswapV2RouterAbi from "./pancakeswapV2Router.json"
 import tokenAbi from "./tokenAbi.json"
 import wbnbAbi from "./wbnbAbi.json"
 import splitContractAbi from "./splitContractAbi.json"
+import splitEthContractAbi from "./splitEthContractAbi.json"
 
 export const contractsAbi = {
   pancakeswapV2RouterAbi,
   tokenAbi,
   wbnbAbi,
-  splitContractAbi
+  splitContractAbi,
+  splitEthContractAbi
 }
